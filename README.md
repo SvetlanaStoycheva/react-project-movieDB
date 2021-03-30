a71006fbf5105461361d88b43ae568ea7cc40c4e
Svetlana Stoycheva <s.stoycheva@gmail.com>
2021-03-30 13:48:14 +0300

    second commit

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 #### React project from John Smilga's React tutorial
 
-- using external MoviesDB API
+- using external OMDb API
 - React context Api to set up the global context
 - React router
 - search form for searching true the movies
--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import Home from './Home'
 import Movie from './SingleMovie'
 
 function App() {
-  return <h2>movie DB starter</h2>
+  return (
+    <Switch>
+      <Route path='/'>
+        <Home />
+      </Route>
+      <Route path='/movies/:id' children={<Movie />}></Route>
+    </Switch>
+  )
 }
 
 export default App
